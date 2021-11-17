ff8f300691c72d7d9c37adaae64bc4e42d1ae607
iagpomponet <iagosilbernagel@hotmail.com>
2021-11-17 10:18:29 -0300

    just moved a function from other file :D

--- deque.js
+++ deque.js
@@ -73,3 +73,29 @@ class Deque {
         return this.#items[this.#count - 1];
     }
 }
+
+function palindromeChecker(aString) {
+    if (aString === undefined || aString === null) {
+        return false;
+    }
+
+    const deque = new Deque();
+    const lowerString = aString.toLocaleLowerCase().split('').join('');
+    let isEqual = true;
+    let firstChar, lastChar;
+
+    for (let i = 0; i < lowerString.length; i++) {
+        deque.addBack(lowerString.charAt(i));
+    }
+
+    while (deque.size() > 1 && isEqual) {
+        firstChar = deque.removeFront();
+        lastChar = deque.removeBack();
+
+        if (firstChar !== lastChar) {
+            isEqual = false;
+        }
+    }
+
+    return isEqual;
+}
--- queue.js
+++ queue.js
@@ -93,31 +93,6 @@ const res = hotPotato(names, 7);
 console.log(res.winner);
 console.log(res.eliminated);
 
-function palindromeChecker(aString) {
-    if (aString === undefined || aString === null) {
-        return false;
-    }
-
-    const deque = new Deque();
-    const lowerString = aString.toLocaleLowerCase().split('').join('');
-    let isEqual = true;
-    let firstChar, lastChar;
-
-    for (let i = 0; i < lowerString.length; i++) {
-        deque.addBack(lowerString.charAt(i));
-    }
-
-    while (deque.size() > 1 && isEqual) {
-        firstChar = deque.removeFront();
-        lastChar = deque.removeBack();
-
-        if (firstChar !== lastChar) {
-            isEqual = false;
-        }
-    }
-
-    return isEqual;
-}
 
 // const queue = new Queue();
 // console.log(queue.isEmpty());
